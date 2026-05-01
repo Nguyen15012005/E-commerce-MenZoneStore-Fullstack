6d6eda38192e83594674aed6d56b483cd9d9af76
TrungNguyen <23640731.nguyen@student.iuh.edu.vn>
2026-05-01 13:00:27 +0700

    function review

---
+++ backend/src/main/java/iuh/fit/backend/repository/CouponRepository.java
@@ -0,0 +1,9 @@
+package iuh.fit.backend.repository;
+
+/**
+ * @author TrungNguyen
+ * @created 5/1/2026
+ * @description
+ */
+public interface CouponRepository {
+}
---
+++ backend/src/main/java/iuh/fit/backend/service/impl/WishlistServiceImpl.java
@@ -0,0 +1,48 @@
+package iuh.fit.backend.service.impl;
+
+import iuh.fit.backend.model.Product;
+import iuh.fit.backend.model.User;
+import iuh.fit.backend.model.Wishlist;
+import iuh.fit.backend.repository.WishlistRepository;
+import iuh.fit.backend.service.WishlistService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author TrungNguyen
+ * @created 5/1/2026
+ * @description
+ */
+@Service
+@RequiredArgsConstructor
+public class WishlistServiceImpl implements WishlistService {
+    private final WishlistRepository wishlistRepository;
+
+    @Override
+    public Wishlist createWishlist(User user) {
+        Wishlist wishlist = new Wishlist();
+        wishlist.setUser(user);
+        return wishlistRepository.save(wishlist);
+    }
+
+    @Override
+    public Wishlist getWishListByUserId(User user) {
+        Wishlist wishlist = wishlistRepository.findByUserId(user.getId());
+        if (wishlist == null) {
+            wishlist = createWishlist(user);
+        }
+        return wishlist;
+    }
+
+    @Override
+    public Wishlist addProductToWishlist(User user, Product product) {
+        Wishlist wishlist = getWishListByUserId(user);
+        if(wishlist.getProducts().contains(product)) {
+            wishlist.getProducts().remove(product);
+        }
+        else {
+            wishlist.getProducts().add(product);
+        }
+        return wishlistRepository.save(wishlist);
+    }
+}
